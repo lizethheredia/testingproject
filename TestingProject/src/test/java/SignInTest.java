ed637f3f5a6bb6519f469b4dcd01fc52ee55f8e1
lheredia <lizeth.heredia@gmail.com>
2017-01-20 14:42:43 -0600

    Comment out a test for interview

--- TestingProject/src/test/java/SignInTest.java
+++ TestingProject/src/test/java/SignInTest.java
@@ -35,27 +35,27 @@ public class SignInTest {
 	  testingApi.getElement(TestAPILocators.SUBMIT_SIGNIN, "NAME").click();
 	  assertThat(testingApi.getElement(TestAPILocators.ALERT1_SIGNIN, "XPATH").getText(), is(equalTo("Invalid email or password."))); 
   }
-//  @Test
-//  public void SignInCreateAccount(){
-//	  testingApi.initialize();
-//	  testingApi.get("https://home.openweathermap.org/users/sign_in");  
-//	  assertThat(testingApi.getElement(TestAPILocators.H3_SIGNIN, "XPATH").getText(), is(equalTo("Sign In")));
-//	  //Click on Create Account
-//	  testingApi.getElement(TestAPILocators.CREATE_ACCOUNT, "XPATH").click();
-//	  assertThat(testingApi.getElement(TestAPILocators.H3_SIGNIN, "XPATH").getText(), is(equalTo("Sign Up")));
-//	  testingApi.getElement(TestAPILocators.USER_SIGNUP, "ID").sendKeys("lizethheredia");
-//	  testingApi.getElement(TestAPILocators.EMAIL_SIGNIN, "ID").sendKeys("lizeth.heredia@gmail.com");
-//	  testingApi.getElement(TestAPILocators.PWD_SIGNIN, "ID").sendKeys("somebody");
-//	  testingApi.getElement(TestAPILocators.USER_PWD_CONF, "ID").sendKeys("somebody");
-//	  testingApi.getElement(TestAPILocators.USER_AGREEMENT, "ID").click();
-//	  testingApi.getElement(TestAPILocators.SUBMIT_SIGNIN, "NAME").click();
-// 	  testingApi.getElement(TestAPILocators.COMPANY, "ID").sendKeys("Wizeline");
-// 	  Select select = new Select(testingApi.getElement(TestAPILocators.PURPOSE, "ID"));
-//	  select.deselectAll();
-//	  select.selectByVisibleText("Advertising");
-//	  testingApi.getElement(TestAPILocators.SAVEHOWANDWHERE, "NAME").click();
-//	  assertThat(testingApi.getElement(TestAPILocators.H3_SIGNIN, "XPATH").getText(), is(equalTo("My Home")));
-//  }
+  @Test
+  public void SignInCreateAccount(){
+	  testingApi.initialize();
+	  testingApi.get("https://home.openweathermap.org/users/sign_in");  
+	  assertThat(testingApi.getElement(TestAPILocators.H3_SIGNIN, "XPATH").getText(), is(equalTo("Sign In")));
+	  //Click on Create Account
+	  testingApi.getElement(TestAPILocators.CREATE_ACCOUNT, "XPATH").click();
+	  assertThat(testingApi.getElement(TestAPILocators.H3_SIGNIN, "XPATH").getText(), is(equalTo("Sign Up")));
+	  testingApi.getElement(TestAPILocators.USER_SIGNUP, "ID").sendKeys("lizethheredia");
+	  testingApi.getElement(TestAPILocators.EMAIL_SIGNIN, "ID").sendKeys("lizeth.heredia@gmail.com");
+	  testingApi.getElement(TestAPILocators.PWD_SIGNIN, "ID").sendKeys("somebody");
+	  testingApi.getElement(TestAPILocators.USER_PWD_CONF, "ID").sendKeys("somebody");
+	  testingApi.getElement(TestAPILocators.USER_AGREEMENT, "ID").click();
+	  testingApi.getElement(TestAPILocators.SUBMIT_SIGNIN, "NAME").click();
+ 	  testingApi.getElement(TestAPILocators.COMPANY, "ID").sendKeys("Wizeline");
+ 	  Select select = new Select(testingApi.getElement(TestAPILocators.PURPOSE, "ID"));
+	  select.deselectAll();
+	  select.selectByVisibleText("Advertising");
+	  testingApi.getElement(TestAPILocators.SAVEHOWANDWHERE, "NAME").click();
+	  assertThat(testingApi.getElement(TestAPILocators.H3_SIGNIN, "XPATH").getText(), is(equalTo("My Home")));
+  }
   
   @BeforeMethod
   public void beforeMethod() {
